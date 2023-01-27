28a1c30ca5eb28b11afb7f31d54eb4d494f7d93b
ElDav1d <dvivoes@yahoo.es>
2023-01-27 19:16:05 +0100

    Create wall

--- javascript/classes/game.js
+++ javascript/classes/game.js
@@ -1,6 +1,7 @@
 class Game {
   constructor() {
     this.character = new Character(initXPosition, yPosition, characterW, characterH);
+    this.wall = new Wall(0, 30);
     this.frames = 1;
     this.isGameOn = true;
   }
@@ -13,9 +14,11 @@ class Game {
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     // animation and actions
+    this.wall.moveWall();
 
     // drawing
     this.character.drawCharacter();
+    this.wall.drawWall()
 
     // recursion
     if (this.isGameOn) {
---
+++ javascript/classes/wall.js
@@ -0,0 +1,18 @@
+class Wall {
+  constructor(positionX, positionY) {
+    this.x = positionX;
+    this.y = positionY;
+    this.w = 600;
+    this.h = 30;
+    this.speed = 1;
+  }
+
+  drawWall = () => {
+    context.fillStyle = "white";
+    context.fillRect(this.x, this.y, this.w, this.h);
+  };
+
+  moveWall = () => {
+    this.y += this.speed;
+  }
+}
